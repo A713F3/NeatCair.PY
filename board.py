1728ddf43019f6335a6c2e53f59e137f82bda706
Ali Efe <65660565+A713F3@users.noreply.github.com>
2022-06-26 22:20:16 +0300

    added car, log render and log logic

--- board.py
+++ board.py
@@ -5,10 +5,18 @@ class Board:
 
         self.log_pos = log_pos
 
-    def move_log(self, velo=1, limit=100):
+    def move_log(self, velo=1, car=100, limit=150):
         self.log_pos += velo
-
+        
+        if self.log_pos >= car and self.log_lane == self.car_lane:
+            self.log_pos = 0
+        
         if self.log_pos >= limit:
             self.log_pos = 0
 
+class Object:
+    def __init__(self, w, h, color):
+        self.w = w
+        self.h = h
+        self.color = color
     
--- render.py
+++ render.py
@@ -1,5 +1,5 @@
 import pygame
-from board import Board
+from board import Board, Object
 
 pygame.init()
 
@@ -27,15 +27,21 @@ COL = 3
 
 SIZE = 200
 PD = 5
+DSIZE = SIZE - 2*PD
 
 WIDTH = COL * SIZE
 HEIGHT = ROW * SIZE
+
+CAR = Object(30, DSIZE//4, (255, 0, 0))
+LOG = Object(15, DSIZE//4, (139, 69, 19))
+LANE = Object(10, DSIZE, (200, 200, 200))
+ROAD = Object(DSIZE, DSIZE, (20, 20, 20))
 
 BOARDS = []
 for r in range(ROW):
     row = []
     for c in range(COL):
-        board = Board(c%2, r%2)
+        board = Board(0, 0)
         row.append(board)
 
     BOARDS.append(row)
@@ -49,15 +55,41 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 0, 0))
+    screen.fill((255, 255, 255))
 
-    cl = 255
     for r in range(ROW):
         for c in range(COL):
             board = BOARDS[r][c]
 
-            pygame.draw.rect(screen, (cl, cl, cl), (c*SIZE+PD, r*SIZE+PD, SIZE-2*PD, SIZE-2*PD))
-            cl -= 25
+            x = c*SIZE+PD
+            y = r*SIZE+PD
+            
+            #Background
+            pygame.draw.rect(screen, ROAD.color, (x, y, ROAD.w, ROAD.h)) 
+
+            #Lane
+            lane_x = x + DSIZE//2 - LANE.w//2
+            pygame.draw.rect(screen, LANE.color, (lane_x, y, LANE.w, LANE.h))
+
+            #Car
+            if board.car_lane == 0:
+                car_x = x + DSIZE//4 - CAR.w//2
+            if board.car_lane == 1:
+                car_x = x + 3*DSIZE//4 - CAR.w//2
+
+            pygame.draw.rect(screen, CAR.color, (car_x, y + 3*CAR.h, CAR.w, CAR.h))
+
+            #Log
+            if board.log_lane == 0:
+                log_x = x + DSIZE//4 - LOG.w//2
+            if board.log_lane == 1:
+                log_x = x + 3*DSIZE//4 - LOG.w//2
+
+            pygame.draw.rect(screen, LOG.color, (log_x, y+board.log_pos, LOG.w, LOG.h))
+
+            #Move Logs
+            board.move_log(car=DSIZE - CAR.h - LOG.h, limit=DSIZE - LOG.h)
+            
 
     pygame.display.update()
 
